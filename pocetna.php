75b3275799dafdd0120f2645637a70b3a5c2fcf8
ElmedinaSupic <45944341+ElmedinaSupic@users.noreply.github.com>
2021-06-14 18:05:26 +0200

    Update pocetna.php

--- pocetna.php
+++ pocetna.php
@@ -41,7 +41,7 @@
 					<small>
 						<i  style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i> 
 						<br>
-						<a href="index.php?logout='1'" style="color: red;">logout</a>
+						<a href="pocetna.php?logout='1'" style="color: red;">logout</a>
 					</small>
 
 				<?php endif ?>
